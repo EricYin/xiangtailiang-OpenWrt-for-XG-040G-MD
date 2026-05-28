f64f0823d7d8334144a551b895501eeae268b1bb
EricYin <public_2011_2011@qq.com>
2026-05-28 10:17:15 +0800

    Update install-patch.sh

--- scripts/install-patch.sh
+++ scripts/install-patch.sh
@@ -1,6 +1,3 @@
-echo "list current patch dir..."
-ls ./patch/target
+echo "Installing Patches..."
 cp -rf ./patch/target/. ./openwrt/target
-ls ./openwrt/target/linux/airoha/dts
-ls ./openwrt/target/linux/airoha/an7581/base-files/lib/upgrade
-cat ./openwrt/target/linux/airoha/dts/an7581-nowifi.dtsi
+cat ./openwrt/target/linux/airoha/an7581/base-files/lib/upgrade/platform.sh
