76bb830e0d29d586157604d3fabd986cc827c0d0
EricYin <public_2011_2011@qq.com>
2026-05-28 09:42:23 +0800

    Update install-patch.sh

--- scripts/install-patch.sh
+++ scripts/install-patch.sh
@@ -1,6 +1,5 @@
-echo "Installing Patches..."
+echo "list current dir..."
 ls
-dir=$(dirname ${realpath $0})
-cp -f "$dir/../patch/target/* openwrt/target/
-ls openwrt/target/linux/airoha/dts
-ls openwrt/target/linux/airoha/an7581/base-files/lib/upgrade
+cp -f "$./patch/target/* ./openwrt/target/
+ls ./openwrt/target/linux/airoha/dts
+ls ./openwrt/target/linux/airoha/an7581/base-files/lib/upgrade
